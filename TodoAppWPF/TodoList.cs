56ebcee862dc6b8365e4c607318435260887647d
Tamana Mohammadi <tmohammadi831@gmail.com>
2025-01-14 06:58:50 +0100

    fixat

---
+++ TodoAppWPF/TodoList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAppWPF
+{
+    /// <summary>
+    /// Hantera Logiken för att lägga till, ta bort och uppdatera TodoItems 
+    /// </summary>
+    public class TodoList
+    {
+        private List<String> tasks = new List<string>();
+        public void AddTask(string task)
+
+        {
+            task.Add(task);
+        }
+        public void RemoveTask(int index )
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+        }
+        public List<string> GetAllTasks()
+
+        {
+            return tasks;
+        }
+    }
+}
